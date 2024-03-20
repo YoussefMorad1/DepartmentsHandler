a9811266af835442ed2d5cedd271b08f4ed820f0
Youssef Morad <youssefmohammed747@gmail.com>
2024-03-21 01:33:45 +0200

    Added the repository for departments at BLL

---
+++ BLL_BusinessLogicLayer/Repositories/DepartmentsRepository.cs
@@ -0,0 +1,53 @@
+﻿using BLL_BusinessLogicLayer.Interfaces;
+using DAL_DataAccessLayer.Data.Contexts;
+using DAL_DataAccessLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL_BusinessLogicLayer.Repositories
+{
+	class DepartmentsRepository : IDepartmentsRepository
+	{
+		private readonly MainContext dbContext;
+
+		public DepartmentsRepository(MainContext dbContext)
+			=> this.dbContext = dbContext;
+
+
+		public int Add(Department department)
+		{
+			dbContext.Add(department);
+			return dbContext.SaveChanges();
+		}
+
+		public int Delete(Department department)
+		{
+			dbContext.Remove(department);
+			return dbContext.SaveChanges();
+		}
+
+		public IEnumerable<Department> GetAll()
+		{
+			return dbContext.Departments.ToList();
+		}
+
+		public Department GetById(int id)
+		{
+			//var department = dbContext.Departments.Local.Where(d => d.Id == id).FirstOrDefault();
+			//if (department == null)
+			//	department = dbContext.Departments.Where(d => d.Id == id).FirstOrDefault();
+			//return department;
+			return dbContext.Departments.Find(id);
+		}
+
+		public int Update(Department department)
+		{
+			dbContext.Update(department);
+			return dbContext.SaveChanges();
+		}
+	}
+}
